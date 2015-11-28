e003d32f07cf1df1203a474e15e7ed7646954982
Carlos Ochoa <cgochoa@acid.cl>
2015-11-28 17:02:44 -0300

    add failing specs

---
+++ spec/lib/simplest_benchmark_spec.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+require 'simplest_benchmark'
+
+RSpec.describe SimplestBenchmark do
+
+		context "with simple calls" do
+
+			it "returns time_spent > 0" do
+				sb_begin("iteration")
+
+					10.times {|i| i*10}
+
+				t = sb_end("iteration")
+				expect(t).to be > 0
+			end
+
+			it "logs correctly" do
+				pending("not implemented yet.")
+    		fail
+			end
+		end
+
+		context "with nested calls" do
+			it "returns coherent time spent" do
+				t,t0 = 0
+
+				sb_begin("iteration1")
+
+					10.times {|i|
+						i*10
+
+						sb_begin("iteration2")
+
+						20.times{|j|
+							j*10
+						}
+
+						t0 = sb_end("iteration2")
+					}
+
+				t = sb_end("iteration")
+				expect(t).to be > t0
+			end
+
+			it "logs correctly" do
+				pending("not implemented yet.")
+    		fail
+			end
+		end
+end
